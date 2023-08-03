644b96f7663bc108850694157d542543fa49e942
Caldion1990 <137450102+Caldion1990@users.noreply.github.com>
2023-08-03 21:55:54 +0300

    Create Task_47.cs

---
+++ Task_47.cs
@@ -0,0 +1,28 @@
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[m, n];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        Random rand = new Random();
+        numbers[i, j] = rand.NextDouble() * 20 -10;
+    }
+}
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++) 
+    {
+        Console.Write($"{numbers[i, j]:F1}   ");
+    }
+    Console.WriteLine();
+}
